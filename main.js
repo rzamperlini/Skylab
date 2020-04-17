01bfd99654a2f03cef14f1ae1387b6faea8cae98
rzamperlini <rzamperlini@gmail.com>
2020-04-17 16:57:21 -0300

    Operações com arrays - concluído

--- main.js
+++ main.js
@@ -1,23 +1,25 @@
-class List{
-    constructor(){
-        this.data = [];
-    }
+const arr = [1,3,4,5,8,9];
 
-    add(data){
-        this.data.push(data);
+const newArr = arr.map(function(item,index){
+    return item + index;
+})
 
-        console.log(this.data);
-    }
-}
+console.log(newArr);
 
-class TodoList extends List{
-   constructor(){
-       super();
-   }
-}
+const sum = arr.reduce(function(total, next){
+    return total + next;
+})
 
-var MinhaLista = new TodoList();
+console.log(sum);
 
-document.getElementById("novoTodo").onclick = function(){
-    MinhaLista.add('Novo TODO');
-}
+const filter = arr.filter(function(item){
+    return item % 2 === 0;
+})
+
+console.log(filter);
+
+const find = arr.find(function(item){
+    return item === 4;
+})
+
+console.log(find);
