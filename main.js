8aca901666cdebfe10f6e61829e9beb787d050e9
rzamperlini <rzamperlini@gmail.com>
2020-04-18 08:54:02 -0300

    Template literals - completed

--- main.js
+++ main.js
@@ -1,46 +1,6 @@
-//REST operator
+//Template literals
 
-const usuario = {
-    nome:"Rodrigo",
-    idade:41,
-    endereco:{
-        rua:"Rua Tito Matos Loureiro.",
-        cidade:"Aracruz",
-        UF:"ES"
-    }
-}
+const nome = "Rodrigo";
+const idade = 41;
 
-const {nome, ...resto} = usuario;
-
-console.log(nome);
-console.log(resto);
-
-const arr = [1,2,3,4,5,6];
-
-const [a,b,c,...d] = arr;
-
-console.log(a);
-console.log(b);
-console.log(d);
-
-function sum(a, b, ...rest){
-
-    var total = rest.reduce((total,next)=> total + next );
-
-    return a + b + total;
-}
-
-console.log(sum(1,2,3,4,5));
-
-
-//SPREAD operator
-
-const usuario1= {
-    nome:"Diego",
-    idade: 23,
-    empresa: "Rocketseat"
-}
-
-const usuario2 = {...usuario1, nome:"Gabriel"};
-
-console.log(usuario2);
+console.log(`Meu nome é ${nome} e tenho ${idade} anos.`)
