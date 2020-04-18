64e0826152796a38f66df72e0c43ef5b774126f4
rzamperlini <rzamperlini@gmail.com>
2020-04-18 15:35:44 -0300

    ES6+ Módulo 01 - Exercício 06 - concluído

--- bundle.js
+++ bundle.js
@@ -1,50 +1,7 @@
 "use strict";
 
-function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
-
-function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
-
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
-
-//ES6+ Módulo 01 - Exercício 05
-//5.1
-
-/* const arr = [1, 2, 3, 4, 5, 6];
-
-const [x,...y] = arr;
-
-console.log(x);
-console.log(y); 
-
-
-function soma(...params) {
-    return params.reduce((total,next)=>total+next);
-}
-console.log(soma(1, 2, 3, 4, 5, 6)); // 21
-console.log(soma(1, 2)); // 3
- */
-//5.2
-var usuario = {
-  nome: 'Diego',
-  idade: 23,
-  endereco: {
-    cidade: 'Rio do Sul',
-    uf: 'SC',
-    pais: 'Brasil'
-  }
-};
-
-var usuario2 = _objectSpread({}, usuario, {
-  nome: 'Gabriel'
-});
-
-console.log(usuario2);
-var endereco = usuario.endereco;
-
-var usuario3 = _objectSpread({}, usuario, {
-  endereco: _objectSpread({}, endereco, {
-    cidade: 'Lontras'
-  })
-});
-
-console.log(usuario3);
+//ES6+ Módulo 01 - Exercício 06
+var usuario = 'Diego';
+var idade = 23;
+console.log('O usuário ' + usuario + ' possui ' + idade + ' anos');
+console.log("O usu\xE1rio ".concat(usuario, " possui ").concat(idade, " anos"));
--- main.js
+++ main.js
@@ -1,36 +1,7 @@
-//ES6+ Módulo 01 - Exercício 05
+//ES6+ Módulo 01 - Exercício 06
 
-//5.1
-/* const arr = [1, 2, 3, 4, 5, 6];
+const usuario = 'Diego';
+const idade = 23;
+console.log('O usuário ' + usuario + ' possui ' + idade + ' anos');
 
-const [x,...y] = arr;
-
-console.log(x);
-console.log(y); 
-
-
-function soma(...params) {
-    return params.reduce((total,next)=>total+next);
-}
-console.log(soma(1, 2, 3, 4, 5, 6)); // 21
-console.log(soma(1, 2)); // 3
- */
-
- //5.2
- const usuario = {
-    nome: 'Diego',
-    idade: 23,
-    endereco: {
-    cidade: 'Rio do Sul',
-    uf: 'SC',
-    pais: 'Brasil',
-    }
-   };
-
-   const usuario2 = {...usuario, nome:'Gabriel'};
-   console.log(usuario2);
-
-   const {endereco} = usuario;
-   
-   const usuario3 = {...usuario, endereco:{...endereco, cidade: 'Lontras'}};
-   console.log(usuario3);
+console.log(`O usuário ${usuario} possui ${idade} anos`);
