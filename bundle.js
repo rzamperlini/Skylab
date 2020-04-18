8e5faf05e1d92d25e7f84ffa0150cbfc1e74f322
rzamperlini <rzamperlini@gmail.com>
2020-04-18 08:20:22 -0300

    Valores padrão em argumentos de arrows functions - concluído

--- bundle.js
+++ bundle.js
@@ -13,3 +13,12 @@ var teste = function teste() {
 };
 
 console.log(teste());
+
+var soma = function soma() {
+  var a = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 3;
+  var b = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 6;
+  return a + b;
+};
+
+console.log(soma(1));
+console.log(soma());
--- main.js
+++ main.js
@@ -7,3 +7,8 @@ console.log(newArr);
 const teste = () => ({nome:"Rodrigo"});
 
 console.log(teste());
+
+const soma = (a=3, b=6) => a + b;
+
+console.log(soma(1));
+console.log(soma());
