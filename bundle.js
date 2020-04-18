0790f3bb4591c87f51577f4e25f19c6ff38c537c
rzamperlini <rzamperlini@gmail.com>
2020-04-18 15:39:13 -0300

    Es6+ Módulo 01 -  Exercício 07

--- bundle.js
+++ bundle.js
@@ -1,7 +1,17 @@
 "use strict";
 
-//ES6+ Módulo 01 - Exercício 06
-var usuario = 'Diego';
+//ES6+ Módulo 01 - Exercício 07
+var nome = 'Diego';
 var idade = 23;
-console.log('O usuário ' + usuario + ' possui ' + idade + ' anos');
-console.log("O usu\xE1rio ".concat(usuario, " possui ").concat(idade, " anos"));
+var usuario = {
+  nome: nome,
+  idade: idade,
+  cidade: 'Rio do Sul'
+};
+var usuario2 = {
+  nome: nome,
+  idade: idade,
+  cidade: 'Rio do Sul'
+};
+console.log(usuario);
+console.log(usuario2);
--- main.js
+++ main.js
@@ -1,7 +1,20 @@
-//ES6+ Módulo 01 - Exercício 06
+//ES6+ Módulo 01 - Exercício 07
 
-const usuario = 'Diego';
+const nome = 'Diego';
 const idade = 23;
-console.log('O usuário ' + usuario + ' possui ' + idade + ' anos');
 
-console.log(`O usuário ${usuario} possui ${idade} anos`);
+const usuario = {
+ nome: nome,
+ idade: idade,
+ cidade: 'Rio do Sul',
+};
+
+const usuario2 = {
+    nome,
+    idade,
+    cidade: 'Rio do Sul'
+};
+
+console.log(usuario);
+console.log(usuario2);
+
