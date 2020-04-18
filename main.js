c508045803f612a6cc2212e2a36c822542002357
rzamperlini <rzamperlini@gmail.com>
2020-04-18 15:33:45 -0300

    ES6+ Módulo 01 - Exercício 05 - concluído

--- main.js
+++ main.js
@@ -1,27 +1,36 @@
-//ES6+ Módulo 01 - Exercício 04
+//ES6+ Módulo 01 - Exercício 05
 
-//4.1
-/* const empresa = {
-    nome: 'Rocketseat',
+//5.1
+/* const arr = [1, 2, 3, 4, 5, 6];
+
+const [x,...y] = arr;
+
+console.log(x);
+console.log(y); 
+
+
+function soma(...params) {
+    return params.reduce((total,next)=>total+next);
+}
+console.log(soma(1, 2, 3, 4, 5, 6)); // 21
+console.log(soma(1, 2)); // 3
+ */
+
+ //5.2
+ const usuario = {
+    nome: 'Diego',
+    idade: 23,
     endereco: {
     cidade: 'Rio do Sul',
-    estado: 'SC',
+    uf: 'SC',
+    pais: 'Brasil',
     }
    };
 
-const {nome, endereco:{cidade, estado}} = empresa;
+   const usuario2 = {...usuario, nome:'Gabriel'};
+   console.log(usuario2);
 
-console.log(nome); // Rocketseat
-console.log(cidade); // Rio do Sul
-console.log(estado); // SC
- */
-
-//4.2
-
-
-function mostraInfo({nome, idade}) {
-    return `${nome} tem ${idade} anos.`;
-   }
+   const {endereco} = usuario;
    
-const aux =  mostraInfo({ nome: 'Diego', idade: 23 })
-console.log(aux);
+   const usuario3 = {...usuario, endereco:{...endereco, cidade: 'Lontras'}};
+   console.log(usuario3);
