5128bef5e7aed601cd9cd7b9824e529eaeffbafb
rzamperlini <rzamperlini@gmail.com>
2020-04-20 14:45:00 -0300

    ES6+ Modulo 04 - Estruturas e estilos.

--- src/main.js
+++ src/main.js
@@ -1,27 +1,2 @@
-//ES6+ - Módulo 03 - Exercício 03
+//ES6+ - Módulo 04 - Aplicação com ES6+
 
-import axios from "axios";
-
-// const buscaUsuario = usuario => {
-//     axios.get(`https://api.github.com/users/${user}`)
-//     .then(response => {
-//     console.log(response.data);
-//     })
-//     .catch(err => {
-//     console.log('Usuário não existe');
-//     });
-//    }
-
-async function buscaUsuario(user){
-
-    try{
-        const response = await axios.get(`https://api.github.com/users/${user}`);
-
-        console.log(response.data);
-    }
-    catch(err){
-        console.log('Usuário não existe.');
-    }
-}
-
-buscaUsuario('diego3g');
